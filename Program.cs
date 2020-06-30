4a81ca40e3be16035db6b9b3d8b37a63468e2bdc
Makoto-dev1 <61596227+Makoto-dev1@users.noreply.github.com>
2020-06-30 16:18:52 -0300

    Commit sem o desafio

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula27_28_29_30_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo = 3;
+            p1.Nome = "Ibanez";
+            p1.Preco = 5500f;
+
+            p1.Cadastrar(p1);
+
+            List<Produto> lista = p1.Ler();
+
+            foreach (Produto item in lista)
+            {
+                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
+        }
+    }
+}
